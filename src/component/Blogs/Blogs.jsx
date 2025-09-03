a433ee9968476602bc42e7be7f6f961db1263a90
Mahabub-Hasan-Bayzid <s2500143@edu.bc.fi>
2025-09-03 10:54:25 +0300

    Change Api directory

--- src/component/Blogs/Blogs.jsx
+++ src/component/Blogs/Blogs.jsx
@@ -1,24 +1,27 @@
 import { useEffect, useState } from "react";
 import Blog from "./Blog/Blog";
-const Blogs = ({handleBookmark,handleRead}) => {
-    const [blogs,setBlogs] = useState([]);
+const Blogs = ({ handleBookmark, handleRead }) => {
+  const [blogs, setBlogs] = useState([]);
 
-    useEffect(()=>{
-        fetch("../../../public/blogs.json").then(res=> res.json()).then(data=>setBlogs(data));
+  useEffect(() => {
+    fetch("../../../blogs.json")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
 
-    },[])
-
-    return (
-        <div className="md:w-2/3">
-            <h1 className="text-4xl">Blogs: {blogs.length}</h1>
-            {
-                blogs.map(blog=> <Blog key={blog.id} blog={blog} 
-                handleBookmark={handleBookmark}
-                handleRead={handleRead}
-                ></Blog>)
-            }
-        </div>
-    );
+  return (
+    <div className="md:w-2/3">
+      <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+      {blogs.map((blog) => (
+        <Blog
+          key={blog.id}
+          blog={blog}
+          handleBookmark={handleBookmark}
+          handleRead={handleRead}
+        ></Blog>
+      ))}
+    </div>
+  );
 };
 
 export default Blogs;
